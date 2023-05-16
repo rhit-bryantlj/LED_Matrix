d550702809bd11dadc0449ad6f3fa34f75fb966d
rhit-bryantlj <bryantlj@rose-hulman.edu>
2023-05-16 11:42:24 -0400

    updated arduino code with publishing successfully

--- LED_Wifi_Beginner_Code/LED_Wifi_Beginner_Code.ino
+++ LED_Wifi_Beginner_Code/LED_Wifi_Beginner_Code.ino
@@ -7,10 +7,14 @@ SoftwareSerial esp8266Serial(2, 3); // RX, TX pins for ESP8266 module
 WiFiEspClient espClient;
 PubSubClient client(espClient);
 
+int loops = 0;
+char numberArray[20];
+
 const char* ssid = "Pike Network";
 const char* password = "47WestRange47";
 const char* mqtt_server = "broker.hivemq.com";
 const int port = 1883;
+const char *topic = "Devices/matrix1/image";
 //const char* mqtt_user = "ledmatrixteam";
 //const char* mqtt_pass = "HivePassForL&J2023";
 
@@ -23,40 +27,48 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Connecting to WiFi...");
     WiFi.begin(ssid, password);
-    delay(5000);
+    delay(2000);
   }
 
   // connect to MQTT broker
   client.setServer(mqtt_server, port);
-  client.setCallback(mqttCallback);
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT broker...");
-    if (client.connect("arduino")) {
-      Serial.println("Connected to MQTT broker");
-    } else {
-      Serial.print("Failed to connect to MQTT broker, rc=");
-      Serial.println(client.state());
-      delay(5000);
-    }
-  }
 }
 
 void loop() {
+  if(!client.connected()){
+    connectToMQTT();
+  } 
   // publish a message to the MQTT broker
-//  client.publish("LEDmatrix/Test/pub", "Hello, world!");
-
-  // subscribe to a topic on the MQTT broker
-  client.subscribe("LEDmatrix/Test/sub");
-
-  // handle MQTT messages
-  client.loop();
+//  itoa(loops, numberArray,10);
+//  Serial.println(numberArray);
+//  client.publish("Devices/matrix1/curLight", numberArray);
+  
+    // handle MQTT messages
+    client.loop();
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallback(char* topic, byte* payload, unsigned int length){
   // handle incoming MQTT message
   Serial.println("Received message from MQTT server:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+}
+
+void connectToMQTT(){
+  Serial.println("Connecting to MQTT broker...");
+    if (client.connect("led-matrix-arduino")) {
+      Serial.println("Connected to MQTT broker");
+      client.setCallback(mqttCallback);
+      // subscribe to a topic on the MQTT broker
+      if(client.subscribe(topic)){
+        Serial.print("Client subscribed to : ");
+        Serial.println(topic);
+      }
+    } else {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.println(client.state());
+      delay(5000);
+    }
 }
